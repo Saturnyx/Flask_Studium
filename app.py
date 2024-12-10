b6f515c0c1e1ca23fd16618bb76d32bdf9c4b37c
Harshal <23sabarimanickam.h@acsians.acsi.edu.sg>
2024-12-10 14:39:57 +0800

    Commented Search funtion.
    Signed-off-by: Harshal <23sabarimanickam.h@acsians.acsi.edu.sg>

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect
 from markupsafe import Markup
 
 version = "2024.0"
@@ -30,6 +30,7 @@ def home():  # put application's code here
     return render_template("index.html", version=version, copyright=author)
 
 
+'''
 @app.route("/search", methods=["GET", "POST"])
 def search():
     author = "Harshal"
@@ -47,7 +48,7 @@ def search():
         return render_template("search.html", copyright=author, content=final_result)
     else:
         return render_template("search.html", copyright=author, content="")
-
+'''
 
 @app.route("/guide")
 def guide():  # put application's code here
